8e942b67470240bab3e4b1572958ed4e147afec9
CoderGirl42 <marissa@galacticsoft.net>
2016-06-05 15:12:24 -0700

    .

--- src/ev3.h
+++ src/ev3.h
@@ -1,19 +1,8 @@
 #ifndef __EV3_H__
 #define __EV3_H__
 
-#include "ev314.h"
-#include <stdio.h>
-#include <stdlib.h>
-//#include <unistd.h>   //_getch
-//#include <termios.h>  //_getch
-//#include <string.h>
-#include <stdint.h>
-#include <fcntl.h>
+//#include "ev314.h"
 #include <time.h>
-//#include <errno.h>
-//#include <math.h>
-//#include <iostream>
-//#include <libusb-1.0/libusb.h>
 
 /* EV314 Constants */
 
